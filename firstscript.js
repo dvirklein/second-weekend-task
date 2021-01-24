11eebcf0612702d7745ee1a075abfeea0e56dd79
dvirklein <77276010+dvirklein@users.noreply.github.com>
2021-01-25 01:17:26 +0200

    final product
    finnished

--- firstscript.js
+++ firstscript.js
@@ -161,80 +161,105 @@ for (let j = 0; j < info.length; j++) {
   info[j].totalTime =
     info[j].finishedAt.getHours() - info[j].startedAt.getHours();
 }
+let body = document.body;
+const newTable = document.createElement("table");
+body.append(newTable);
+const newRow = document.createElement("tr");
+newTable.append(newRow);
+const newData1 = document.createElement("th");
+newData1.innerText = "started-at";
+const newData2 = document.createElement("th");
+newData2.innerText = "finished-at";
+const newData3 = document.createElement("th");
+newData3.innerText = "tasks given";
+const newData4 = document.createElement("th");
+newData4.innerText = "tasks-finished";
+const newData5 = document.createElement("th");
+newData5.innerText = "topic";
+const newData6 = document.createElement("th");
+newData6.innerText = "total-time";
+const newData7 = document.createElement("th");
+newData7.innerText = "tasks finished-precent";
+newRow.append(
+  newData1,
+  newData2,
+  newData3,
+  newData4,
+  newData5,
+  newData6,
+  newData7
+);
 
-document.write(`<table>
-<tr>
-<th>Started time</th>
-<th>Finished at</th>
-<th>Tasks given</th>
-<th>Tasks finished</th>
-<th>Topic</th>
-<th>Total time</th>
-<th>Tasks finished Precent</th>
-</tr>`);
 for (props of info) {
-  document.write(`<tr>      
-  <td >
-  ${textFormatForDate(
+  let newRow1 = document.createElement("tr");
+  newTable.append(newRow1);
+  let newData8 = document.createElement("td");
+  newData8.innerText = textFormatForDate(
     props["startedAt"].getHours(),
     props["startedAt"].getMinutes()
-  )}
-  </td>
-  <td>
-  ${textFormatForDate(
+  );
+  newRow1.append(newData8);
+
+  let newData9 = document.createElement("td");
+  newData9.innerText = textFormatForDate(
     props["finishedAt"].getHours(),
     props["finishedAt"].getMinutes()
-  )}
-  </td>
-  <td>
-  ${props["tasksGiven"]}
-  </td>
-  <td style=${tasksColorChange(props["tasksFinished"])}>
-  ${props["tasksFinished"]}
-  </td>
-  <td>
-  ${props["topic"]}
-  </td>
-  <td>
-  ${props.totalTime}
-  </td>
-  <td style=${pColor(props["tasksFinishedPrecent"])}>
-  ${(props["tasksFinishedPrecent"] += "%")}
-  </td>
-  </tr>`);
-}
-document.write(`</table>`);
+  );
+  newRow1.append(newData9);
 
-function tasksColorChange(str) {
-  if (str <= 2) {
-    return "background-color:grey;";
-  } else if (str > 2 && str <= 5) {
-    return "background-color:orange;";
-  } else if (5 < str) {
-    return "background-color:red;";
+  let newData10 = document.createElement("td");
+  newData10.innerText = props["tasksGiven"];
+  newRow1.append(newData10);
+
+  let newData11 = document.createElement("td");
+  newData11.style = tasksColorChange(props["tasksFinished"]);
+  newData11.innerText = props["tasksFinished"];
+  newRow1.append(newData11);
+
+  let newData12 = document.createElement("td");
+  newData12.innerText = props["topic"];
+  newRow1.append(newData12);
+
+  let newData13 = document.createElement("td");
+  newData13.innerText = props["totalTime"];
+  newRow1.append(newData13);
+
+  let newData14 = document.createElement("td");
+  newData14.style = pColor(props["tasksFinishedPrecent"]);
+  newData14.innerText = props["tasksFinishedPrecent"] += "%";
+  newRow1.append(newData14);
+
+  function tasksColorChange(str) {
+    if (str <= 2) {
+      return "background-color:grey;";
+    } else if (str > 2 && str <= 5) {
+      return "background-color:orange;";
+    } else if (5 < str) {
+      return "background-color:red;";
+    }
   }
-}
-function pColor(num) {
-  if (num <= 50) {
-    return "background-color:purple;";
+  function pColor(num) {
+    if (num <= 50) {
+      return "background-color:purple;";
+    }
+    if (num > 50 && num <= 75) {
+      return "background-color:blue;";
+    }
+    if (num > 75) {
+      return "background-color:yellow;";
+    }
   }
-  if (num > 50 && num <= 75) {
-    return "background-color:blue;";
+  function textFormatForDate(hours, mins) {
+    let result = "";
+    if (hours < 10) {
+      result += "0" + hours; // "08"
+    } else {
+      result += hours;
+    }
+    result += ":"; // "08:"
+    if (mins < 10) {
+      result += "0" + mins; // "08:09"
+    } else result += mins;
+    return result;
   }
-  if (num > 75) {
-    return "background-color:yellow;";
-  }
-}
-function textFormatForDate(hours, mins) {
-  let result = "";
-  if (hours < 10) {
-    result += "0" + hours; // "08"
-  } else {
-    result += hours;
-  }
-  result += ":"; // "08:"
-  if (mins < 10) {
-    result += "0" + mins; // "08:09"
-  } else result += mins;
-  return result;
 }
